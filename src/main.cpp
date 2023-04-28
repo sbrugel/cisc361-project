234b13e8f0469f810526c0f30bd7a79b5da384f7
Simon Brugel <sbrugel62@gmail.com>
2023-04-28 14:18:08 -0400

    feat: barebones for input processing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, CISC361!" << std::endl;
+    // system properties
+    int time; // initialize to starting time; increment by one unit
+    int totalMemory;
+    int availableMemory;
+    int totalDevices;
+    int availableDevices;
+    int timeQuantum;
+
+    // reading the input to get all properties
+    std::ifstream infile("../inputs/i0.txt"); // make sure input folder is placed in the root directory
+    std::string line; // currently-read line
+    if (infile.is_open())
+    {
+        while (std::getline(infile, line))
+        {
+            // TODO: process input instead of printing
+            std::cout << line << std::endl;
+        }
+        infile.close();
+    }
+    else
+    {
+        std::cout << "Unable to open file." << std::endl;
+        return 0;
+    }
+    
     return 0;
 }
