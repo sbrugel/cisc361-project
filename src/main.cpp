89d5ce1536928e2d28d847d8700a7a8a77312a0c
Simon Brugel <sbrugel62@gmail.com>
2023-04-29 10:31:23 -0400

    feat: command parsing and basic argument validation

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 
 #include "utility/String.h"
 
+bool validNumArgs(int allowedNumArgs, std::vector<std::string>& args) {
+    return args.size() == allowedNumArgs + 1; // includes the command
+}
+
 int main() {
     // system properties
     int time; // initialize to starting time; increment by one unit
@@ -22,9 +26,37 @@ int main() {
             // TODO: process input instead of printing
             std::cout << line << std::endl;
 
-            auto words = string::split(line, ' ');
+            auto words = string::split(line, ' '); // std::vector<std::string>
             for (const auto& word : words) {
                 std::cout << word << std::endl;
+            }
+
+            std::string command = words[0]; // get the first character, this corresponds to the "command"
+            if (command == "C") {
+                std::cout << "OS initialization" << std::endl;
+                if (validNumArgs(4, words)) {
+                    std::cout << "Valid args! :D" << std::endl;
+                }
+            } else if (command == "A") {
+                std::cout << "Job arrival" << std::endl;
+                if (validNumArgs(6, words)) {
+                    std::cout << "Valid args! :D" << std::endl;
+                }
+            } else if (command == "Q") {
+                std::cout << "Device request" << std::endl;
+                if (validNumArgs(3, words)) {
+                    std::cout << "Valid args! :D" << std::endl;
+                }
+            } else if (command == "L") {
+                std::cout << "Device release" << std::endl;
+                if (validNumArgs(3, words)) {
+                    std::cout << "Valid args! :D" << std::endl;
+                }
+            } else if (command == "D") {
+                std::cout << "Display" << std::endl;
+                if (validNumArgs(1, words)) {
+                    std::cout << "Valid args! :D" << std::endl;
+                }
             }
         }
         infile.close();
